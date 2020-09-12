be4b64d407f7eddfab916c9e947d54569dbd9c96
Irvingperez <irving.perez2@g.austincc.edu>
2020-09-11 22:55:38 -0500

    Homework_sep11

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,21 @@
 //Write the include statement for types.h here
-
+#include<iostream>
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
+using std::cout;	using std::cin;	
 
 
 int main() 
 {
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	cout<<num;
+	int result = (multiply_numbers(num))
+	cout<<"Result is:  "<<result;
+	int num1 = 4;
+	multiply_numbers(num1)
+	cout<<"Result is:  "<<result;
 
 	return 0;
 }
--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -2,13 +2,23 @@
 #include "types.h"
 
 //DON'T ERASE THIS
+
+
 int sum_numbers(int num1, int num2)
+
 {
 	return num1 + num2;
+
 }
+//my program it doesnt work, I was trying to see in your videos if can get the aswerd but i coulndt solve it.
+// 
+int multiply_numbers(int num1, int num2)
+int num2 = 5
 
+{
+	return num1 * num2; 
 
-
+}
 
 
 
