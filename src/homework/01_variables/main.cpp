994ac16190e42cf17cdf1e9ae83b5ac58bac2085
Irvingperez <irving.perez2@g.austincc.edu>
2020-09-18 23:21:12 -0500

    main Bill Restaurant

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,41 @@
 ﻿//write include statements
+#include<iostream>
+#include<variables.h>
+
 
 
 //write namespace using statement for cout
+using std::cout;	using std::cin;
 
+
+int main()
+{
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+	cout<<"Enter meal amount: ";
+	cin>>meal_amount;
+	get_sales_tax_amount(meal_amount);
+	cout<<"Enter tip: ";
+	cin>>tip_rate;
+	tip_amount = get_tip_amount(meal_amount,  tip_rate);
+	total =  tip_amount + tax_amount + meal_amount;
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<<"Total: "<<total<<"\n";
+	return 0;
+
+}
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
-int main()
-{
+//int main()
+//{
 	
 
-	return 0;
-}
+//	return 0;
+//}
