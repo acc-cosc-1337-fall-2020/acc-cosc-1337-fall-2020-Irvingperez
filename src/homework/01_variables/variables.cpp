d1f5cd7e42161d6bfa1a01e49658c7fe8257a5db
Irvingperez <irving.perez2@g.austincc.edu>
2020-09-18 22:59:15 -0500

    Bill restaurant

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -9,6 +9,8 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
-
-
-
+const double tax_rate = 0.0675
+{
+	return tax_rate * meal_amount;
+	return meal_amount * tip_rate;
+}
