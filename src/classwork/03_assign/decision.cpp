15d69ce11eeb78a5ad7be7beace1fe214cbdd9fb
Irvingperez <irving.perez2@g.austincc.edu>
2020-09-25 22:44:12 -0500

    HW 2 If and switch functions

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,66 @@
 //cpp
+#include "decision.h"
+
+using std::string;
+
+string get_letter_grade_using_if(int grade)
+{
+    std::string letter_grade;
+    if(grade > 90 && grade < 100)
+    {
+        letter_grade = "A";
+
+    }
+    else if (grade > 79 && grade < 90)
+    {
+        letter_grade = "B";
+
+    }
+    else if (grade > 69 && grade <80)
+    {
+        letter_grade = "C";
+
+    }
+    else if (grade > 59 && grade <70)
+    {
+        letter_grade = "D";
+
+    }
+    else
+    {
+        letter_grade = "F";
+    }
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    std::string grade_switch;
+    grade = grade / 10;
+
+    switch(grade)
+    {
+      case 9:
+      grade_switch = "A";
+      break;
+      case 10:
+      grade_switch = "A";
+      break;
+      case 8:
+      grade_switch = "B";
+      break;
+      case 7:
+      grade_switch = "C";
+      break;
+      case 6:
+      grade_switch = "D";
+      break;
+      default:
+      grade_switch = "F";
+      break;
+
+      return grade_switch;
+
+
+    }
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,27 @@
 //Write the include statement for decisions.h here
-
+#include"decision.h"
+#include<iostream>
 //Write namespace using statements for cout and cin
+using std::cin;
+using std::cout;
 
 int main() 
 {
+	int grade;
 
-	return 0;
+	cout<<"Enter a grade: ";
+	cin>>grade;
+
+	if (grade<101 && grade>=0)
+	{
+		cout<<"\n Grade for "<<grade<<" is: "<<get_letter_grade_using_if(grade)<<" using IF function\n";
+		cout<<"\n Grade for "<<grade<<" is: "<<get_letter_grade_using_switch(grade)<<" using SWITCH function\n";
+		
+	}
+	else 
+	{
+		cout<<"Wrong value / out of range";
+	}
+		return 0;
 }
 
