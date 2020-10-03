606092aef82a42db95e76593615f66e9d05f3e96
Irvingperez <irving.perez2@g.austincc.edu>
2020-10-02 20:20:09 -0500

    HW 3 factorial

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,15 @@
 //cpp
+#include"loops.h"
+
+int factorial(int num)
+{
+    int factorial = 1;
+    int i;
+    for (i=1; i < num; i++)
+    {
+        factorial += factorial * i;
+
+    }
+    return factorial;
+}
+
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,11 @@
 //write includes statements
+#include<iostream>
+#include"loops.h"
+
+
 
 //write using statements for cin and cout
-
+using std::cout;	using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -9,7 +13,34 @@ a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
+
 {
+	char choice;
+
+	do{
+		
+		cout<<"Enter a number between 1 and 10: "<<"\n";
+		int num;
+		int total;
+		cin>>num;
+
+
+		while(!(num >=1 && num <=10))
+		{
+			cout<<"Out of range, pick between 1 - 10\n";
+			cout<<"Enter a number between 1 and 10: "<<"\n";
+			cin>>num;
+
+		}
+			
+		total = factorial(num);
+		cout<<"Factorial number for "<<num<<" is: "<<total<<"\n";
+
+		cout<<"Continue? press any key or 'n' to exit the program\n";
+		cin>>choice;
+
+
+	}while(choice != 'n');
 
 	return 0;
 }
