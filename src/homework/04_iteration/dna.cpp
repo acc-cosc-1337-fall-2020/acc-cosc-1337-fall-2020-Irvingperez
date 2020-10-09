b56202b0d4edee24131cd107b7a7a8b5f08631eb
Irvingperez <irving.perez2@g.austincc.edu>
2020-10-09 15:47:47 -0500

    HW 4 DNA

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+
+using std::string;
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +9,41 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna) 
+{
+    int counter = 0;
 
+    for (std::size_t i = 0; i < dna.size(); ++i) 
+    {
+        if (dna[i] == 'C' || dna[i] == 'G')
+         {
+            counter++;
+        }
+    }
 
+    return (double)counter/dna.size();
 
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+string get_reverse_string(string dna) 
+{
+    int len = dna.length();
+    int n = len-1;
+    for (int i = 0; i < (len/2); i++) {
+        char temp;
+        temp = dna[i];
+        dna[i] = dna[n];
+        dna[n]= temp;
+        n=n-1;
+    }
+
+    return dna;
+}
 
 
 /*
@@ -28,3 +57,30 @@ c. return string
 
 */
 
+
+
+string get_dna_complement(string dna)
+{
+    string reverse = get_reverse_string(dna);
+    for (int i = 0; i < reverse.length(); i++)
+    {
+        switch (reverse[i])
+        {
+            case 'A':
+                reverse[i] = 'T';
+                break;
+            case 'T':
+                reverse[i] = 'A';
+                break;
+            case 'C':
+                reverse[i] = 'G';
+                break;
+            case 'G':
+                reverse[i] = 'C';
+                break;
+            default:
+                break;
+        }
+    }
+    return reverse;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,11 @@
 //write include statements
+#include<iostream>
+#include<string>
+#include"dna.h"
 
 //write using statements
-
+using std::cout;	using std::cin;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +16,46 @@ user enters a y or Y.
 */
 int main() 
 {
+	char continue_yes_or_no;
+	int prompt;
+	string dna;
+	double result_gc;
+	string result_complement;
+	
+cout<<"Type 'Y' to continue: ";
+  cin>>continue_yes_or_no;
+
+  while(continue_yes_or_no == 'y' || continue_yes_or_no == 'Y')
+  {
+	  cout<<"Options: Press 1 for GC content or Press 2 for DNA complement: ";
+	  cin>>prompt;
+	
+    if (prompt == 1)
+    {
+      cout<<"Enter the DNA information : ";
+      cin>>dna;
+      result_gc = get_gc_content(dna);
+      cout<<"The GC result is: "<<result_gc<<'\n';
+      cout<<"Pres 'Y' to start over or any key to exit. ";
+      cin>>continue_yes_or_no;
+    }
+
+    else if (prompt == 2)
+    {
+      cout<<"Enter the DNA information: ";
+      cin>>dna;
+      result_complement = get_dna_complement(dna);
+      cout<<"The complement is: "<<result_complement<<'\n';
+      cout<<"Pres 'Y' to start over or any key to exit.  ";
+      cin>>continue_yes_or_no;
+    }
+
+    else
+    {
+      cout<<"Option does not exist  ";
+      
+    }
+  }
+
 	return 0;
 }
