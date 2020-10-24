c7bce374a36d301980f9839f28bffeae63600f96
Irvingperez <irving.perez2@g.austincc.edu>
2020-10-24 10:14:51 -0500

    HW 5 TicTacToe

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,30 @@
 //h
+#include<iostream>
+#include<vector>
+#include<string>
+
+using std::vector;
+using std::string;
+
+
+#ifndef TicTacToe_H
+#define TicTacToe_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player()const;
+    void display_board()const;
+
+private:
+    bool check_board_full()const;
+    void set_next_player();
+    string player;
+    vector<string> pegs{9, " "};
+    void clear_board();
+};
+
+#endif 
